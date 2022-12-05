c9821f0eddcdd12d9d9fa8c5202f1418af3620d6
Sanket Darwante <sanketdarwante6@gmail.com>
2022-12-05 01:51:05 -0500

    Update data-scrapper.py

--- data-scrapper.py
+++ data-scrapper.py
@@ -1,38 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 import pymongo
+
+import ScamProfileAnalyzer
 from GoFundScrapper import MyWebScraper
 from GoFundScrapper import scrap_profile_data
+from PROPERTIES import FIREBASE_DB_URL,FIREBASE_CERTIFICATE, DB_PASSWORD
+import firebase_admin
+from firebase_admin import db
 
-DB_URL = 'mongodb+srv://medscamscapper:<password>@med-scrap-data.mjhhsle.mongodb.net/?retryWrites=true&w=majority'
+
+DB_URL = 'mongodb+srv://medscamscapper:' + DB_PASSWORD + '@med-scrap-data.mjhhsle.mongodb.net/?retryWrites=true&w=majority'
+
+cred_obj = firebase_admin.credentials.Certificate(FIREBASE_CERTIFICATE)
+app = firebase_admin.initialize_app(cred_obj);
 
 
-def scrape_quotes():
-    more_links = True
-    page = 1
-    quotes = []
-    while (more_links):
-        markup = requests.get(f'http://quotes.toscrape.com/page/{page}').text
-        soup = BeautifulSoup(markup, 'html.parser')
-        for item in soup.select('.quote'):
-            quote = {}
-            quote['text'] = item.select_one('.text').get_text()
-            quote['author'] = item.select_one('.author').get_text()
-            tags = item.select_one('.tags')
-            quote['tags'] = [tag.get_text() for tag in tags.select('.tag')]
-            quotes.append(quote)
-
-        next_link = soup.select_one('.next > a')
-
-        # print which page was scraped
-        print(f'scraped page {page}')
-
-        # check if the next link element exists
-        if (next_link):
-            page += 1
-        else:
-            more_links = False
-    return quotes
 
 def dumpScarpData(data, collection):
     client = pymongo.MongoClient(DB_URL)
@@ -43,19 +26,27 @@ def dumpScarpData(data, collection):
     except Exception as e:
         print(f'an error occurred {collection} were not stored to db', e)
 
-def getScarpData(collection):
+
+def getScarpData(collection, limitValue=-1):
     client = pymongo.MongoClient(DB_URL)
     db = client.db[f'{collection}']
     foundData = []
     try:
-        foundData = db.find()
+
+        if limitValue == -1:
+            foundData = db.find()
+        else:
+            foundData = db.find().limit(limitValue)
+
     except Exception as e:
         print(f'an error occurred {collection} were not stored to db', e)
 
     return foundData
+
+
 # Press the green button in the gutter to run the script.
 
-def get_fund_url():
+def process_fund_url():
     # medical url only
     searchResultLink = MyWebScraper('https://www.gofundme.com/s?q=&c=11')
     fundRaiserLinks = []
@@ -65,19 +56,51 @@ def get_fund_url():
 
     dumpScarpData(fundRaiserLinks, 'fundraiser-link')
 
-def get_fund_profile():
 
+def process_fund_profile():
     urls = getScarpData('fundraiser-link')
 
     links = []
     for url in urls:
-        links.append('https://www.gofundme.com'+url['link'])
-    # print(links)
-    # links = ['https://www.gofundme.com/f/matt-summers?qid=43ccfcca04466864bbdc550e384c205c']
+        links.append('https://www.gofundme.com' + url['link'])
 
     for url in links:
         dumpScarpData([scrap_profile_data(url)], 'fundraiser-profile')
 
-if __name__ == '__main__':
-    # get_fund_url()
-    get_fund_profile()
+
+def process_analyzed_data():
+
+    for data in getScarpData('fundraiser-profile', 5):
+        scamLinkData = ScamProfileAnalyzer.analyze_profile(data)
+
+    # push_link_firebase(scamLinkData)
+
+def push_link_firebase(linkData):
+    print('firebase: pushing data links')
+    ref = db.reference("/")
+    link_data_ref = ref.child('scam-url')
+
+    new_link_data_ref = link_data_ref.push()
+    new_link_data_ref.set({
+        'url': 'example.com',
+    })
+
+    for link in linkData:
+        new_link_data_ref.push().set({
+            'url': link
+        })
+    print('firebase: data pushed successfully')
+
+
+# if __name__ == '__main__':
+#     inputSelection = int(input('Enter Selection:\n 1. FETCH FUND URL\n 2. FETCH FUND PROFILE\n 3. Analyze FUND DATA\n'))
+#
+#     if inputSelection == 1:
+#         process_fund_url()
+#     elif inputSelection == 2:
+#         process_fund_profile()
+#     elif inputSelection == 3:
+#         process_analyzed_data()
+
+
+push_link_firebase('example2.com')
